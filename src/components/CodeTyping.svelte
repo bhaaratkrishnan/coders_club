6a5d6a765b109c47a0af9f3fdbfb9853dd7e1f0d
bhaarat krishnan <bhaaratkrishnan29113@gmail.com>
2023-04-07 14:24:06 +0530

    navbar v1.0

--- src/components/CodeTyping.svelte
+++ src/components/CodeTyping.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount } from "svelte";
-  const codeContentArray = ["'hello world'", "'namaste'", "'Good Day !'"];
+  const codeContentArray = ["'hello world'", "'வணக்கம்'", "'Namasate'","'Good Day !'"];
   let codeContent = "";
   let arrayIndex = 0;
   let charIndex = 0;
@@ -17,7 +17,6 @@
     }
   }
   function eraseText() {
-
     if (codeContent.length > 0) {
       codeContent = codeContent.substring(0, charIndex - 1);
       charIndex -= 1;
@@ -35,22 +34,22 @@
   });
 </script>
 
-<div class="flex flex-col justify-center space-y-8">
+<div class="flex flex-col justify-center w-5/6 space-y-8">
   <!-- editor part  -->
   <div class="flex flex-col rounded-xl bg-[#1e1e1e]">
     <!-- top bar of editor  -->
     <div
-      class="flex flex-row bg-[#282828] space-x-4 py-3 px-4 justify-start items-center rounded-xl"
+      class="flex flex-row  bg-[#282828] space-x-4 py-2 px-4 justify-start items-center rounded-xl"
     >
       <!-- icons  -->
       <div class="container w-4 h-4 bg-red-500 rounded-full" />
       <div class="container w-4 h-4 bg-yellow-500 rounded-full" />
       <div class="container w-4 h-4 bg-green-500 rounded-full" />
-      <div class="text-gray-500 font-normal text-4xl px-12">+</div>
+      <div class="text-gray-500 font-normal text-4xl hidden md:block pl-10">+</div>
     </div>
     <!-- text section for editor  -->
     <div
-      class="flex flex-col px-4 py-3 font-semibold text-xl font-mono tracking-wide"
+      class="flex flex-col px-4 py-3 font-semibold text-lg md:text-xl font-mono tracking-wide"
     >
       <!-- include stdio  -->
       <div class="">
@@ -64,7 +63,7 @@
         <span class="text-[#dcdcaa]">&lbrace;</span>
       </div>
       <!-- printf statement  -->
-      <div class="indent-8 text-white">
+      <div class="indent-8 text-white break-words">
         <span class="text-[#dcdcaa]">printf</span><span class="text-white"
           >&lpar;</span
         ><span class="text-[#ce9178]">{codeContent}</span><span
@@ -85,10 +84,10 @@
   <div class="flex flex-col rounded-xl bg-neutral-800">
     <!-- top bar of editor  -->
     <div
-      class="flex flex-row bg-neutral-900 justify-end space-x-4 py-4 px-4 rounded-xl"
+      class="flex flex-row bg-neutral-900 justify-end items-center space-x-4 py-4 px-4 rounded-xl"
     >
       <!-- icons  -->
-      <div class="text-neutral-200 text-lg pr-60 font-medium">
+      <div class="text-neutral-200 text-lg hidden md:block md:pr-60 font-medium">
         codersclub@codersclub-linux/~
       </div>
       <div class="container w-4 h-4 bg-red-500 rounded-full" />
@@ -104,9 +103,9 @@
           >gcc -o Main main.c && ./Main</span
         >
       </div>
-      <div class="text-green-600 text-base">
+      <div class="text-green-600 text-base break-words">
         codersclub@codersclub-linux/~$ <span class="text-white"
-          >{codeContent}</span
+          >{codeContent.substring(1,codeContent.length-1)}</span
         >
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		
+		extend: {
+			colors: {
+				"logoBlue" : "#233dff"
+			},
+		},
 	},
 	plugins: [],
 }
