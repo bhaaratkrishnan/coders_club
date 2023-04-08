d2bbcd1df6db582e21dc0530ec8e5b961483d213
bhaarat krishnan <bhaaratkrishnan29113@gmail.com>
2023-04-08 09:48:16 +0530

    objectives and about us gradient text design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,19 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		
+
 		extend: {
 			colors: {
-				"logoBlue" : "#233dff"
+				"logoBlue": "#233dff"
 			},
-			keyframes : {
-				borderPulse: {
-					"0%": {transform: "border-width: 0px"},
-					"100%":{transform:"border-width: 1px"}
-				}
-			},
+
 			animation: {
-				borderPulse: "borderPulse 2s infinite"
+				pulseFast: "pulse 1s linear infinite"
 			}
 		},
 	},
