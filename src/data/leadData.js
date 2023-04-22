34d7ac1519168c77d4b7e525c07e97f14e105c28
bhaarat krishnan <bhaaratkrishnan29113@gmail.com>
2023-04-22 20:33:16 +0530

    new website

---
+++ src/data/leadData.js
@@ -0,0 +1,154 @@
+export default function leadData() {
+  return [
+    {
+      title: "Technical Lead",
+      leads: [
+        {
+          name: "Rohith C",
+          social: "",
+          imgLink: "/leads/rohith.jpg",
+        },
+      ],
+    },
+    {
+      title: "Competitive Programming Lead",
+      leads: [
+        {
+          name: "Bharanidharan G",
+          social: "",
+          imgLink: "/leads/bharani.jpg",
+        },
+        {
+          name: "Praiseline J",
+          social: "",
+          imgLink: "/leads/praiseline.jpg",
+        },
+      ],
+    },
+    {
+      title: "Frontend Development Lead",
+      leads: [
+        {
+          name: "Pooja Sri K",
+          social: "",
+          imgLink: "/leads/pooja.jpg",
+        },
+        {
+          name: "Mohammed Anees",
+          social: "",
+          imgLink: "/leads/anees.jpg",
+        },
+      ],
+    },
+    {
+      title: "Backend Development Lead",
+      leads: [
+        {
+          name: "Gayathri R",
+          social: "http://linkedin.com/in/gayathri-r-9b2280218",
+          imgLink: "/leads/gayathri.jpg",
+        },
+        {
+          name: "Harini P",
+          social: "http://linkedin.com/in/harini-purushoth-547375212",
+          imgLink: "/leads/harini.jpg",
+        },
+      ],
+    },
+    {
+      title: "Machine Learning Lead",
+      leads: [
+        {
+          name: "Aadhithya R",
+          social: "",
+          imgLink: "/leads/aadhithya.jpg",
+        },
+        {
+          name: "Sujesh G",
+          social: "",
+          imgLink: "/leads/sujesh.jpg",
+        },
+      ],
+    },
+    {
+      title: "App Development Lead",
+      leads: [
+        {
+          name: "Sriram R",
+          social: "https://www.linkedin.com/in/sri-ram-1020131b3",
+          imgLink: "/leads/sriram.jpg",
+        },
+        {
+          name: "Suvarna Narayanan",
+          social: "",
+          imgLink: "/leads/suvarnanarayanan.jpg",
+        },
+      ],
+    },
+    {
+      title: "Cyber Security Lead",
+      leads: [
+        {
+          name: "Muktheeswaran V",
+          social: "",
+          imgLink: "/leads/muktheeswaran.jpg",
+        },
+        {
+          name: "Ashik M",
+          social: "",
+          imgLink: "/leads/ashik.jpg",
+        },
+      ],
+    },
+    {
+      title: "UI/UX and Design Lead",
+      leads: [
+        {
+          name: "Nithyasree I",
+          social: "https://www.linkedin.com/in/nithyasree-i-b61352211",
+          imgLink: "/leads/nithyasree.jpg",
+        },
+        {
+          name: "Prashithaa Abhirami",
+          social: "",
+          imgLink: "/leads/prashithaa.jpg",
+        },
+      ],
+    },
+    {
+      title: "Hospitality and SponsorShip Lead",
+      leads: [
+        {
+          name: "Priyadharshini T",
+          social: "https://www.linkedin.com/in/priya-parthasarathy-bb3729214",
+          imgLink: "/leads/priyadharshini.jpg",
+        },
+        {
+          name: "Swetha",
+          social: "",
+          imgLink: "/leads/swetha.jpg",
+        },
+      ],
+    },
+    {
+      title: "Event Coordinators",
+      leads: [
+        {
+          name: "Kishore Babu M S",
+          social: "https://www.linkedin.com/in/kishore-babu-17ba841aa",
+          imgLink: "/leads/kishore.jpg",
+        },
+        {
+          name: "Roshan G K",
+          social: "https://www.linkedin.com/in/roshan-g-k-aa3aa5249",
+          imgLink: "/leads/roshan.jpg",
+        },
+        {
+          name: "Lakshmi A",
+          social: "",
+          imgLink: "/leads/lakshmi.jpg",
+        },
+      ],
+    },
+  ];
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,5 +21,31 @@ module.exports = {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#2563eb",
+
+					"secondary": "#ea580c",
+
+					"accent": "#1FB2A5",
+
+					"neutral": "#191D24",
+
+					"base-100": "#111827",
+
+					"info": "#3ABFF8",
+
+					"success": "#36D399",
+
+					"warning": "#FBBD23",
+
+					"error": "#F87272",
+				},
+			},
+		],
+	},
 }
